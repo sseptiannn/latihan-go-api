38e3cf2d4e659a5b83672b073643409be9750af4
Septian Pratama <septianpratama18@gmail.com>
2021-02-07 11:10:23 +0700

    membuat homepage & awalan createproducts

--- main.go
+++ main.go
@@ -2,14 +2,62 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/shopspring/decimal"
 )
 
 var db *gorm.DB
 var err error
 
-func main() {
-	fmt.Println("Suekkkkk")
+// Product is a representation of a product
+type Product struct {
+	ID    int             `json:"id"`
+	Code  string          `json:"code"`
+	Name  string          `json:"name"`
+	Price decimal.Decimal `json:"price" sql:"type:decimal(16,2)"`
+}
 
+// Result is an array of product
+type Result struct {
+	Code    int         `json:"code"`
+	Data    interface{} `json:"data"`
+	Message string      `json:"message"`
+}
+
+func main() {
+	db, err = gorm.Open("mysql", "root:1223334444@/go_rest_api_crud?charset=utf8&parseTime=True")
+
+	if err != nil {
+		log.Println("Connection failed", err)
+	} else {
+		log.Println("Connection estabilished")
+	}
+
+	db.AutoMigrate((&Product{}))
+	handleRequests()
+
+}
+
+func handleRequests() {
+	log.Println("Start the development server at http://127.0.0.1:9999")
+
+	myRouter := mux.NewRouter().StrictSlash(true)
+
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/api/products", creareProduct).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":9999", myRouter))
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Welcome Oppa!")
+}
+
+func creareProduct(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "ini create product")
 }
